b59334ad67737f1057cb63774e0cd6fb61c18395
john kenneth furog <furogjohnkenneth@gmail.com>
2018-04-26 14:14:19 +0800

    Add project files.

---
+++ POC_BuildScript/POC_BuildScript/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace POC_BuildScript
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            lbl.Text = Util.key;
+		}
+	}
+}
---
+++ POC_BuildScript/POC_BuildScript/Util.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POC_BuildScript
+{
+    public class Util
+    {
+        public const string key = "test";
+    }
+}
