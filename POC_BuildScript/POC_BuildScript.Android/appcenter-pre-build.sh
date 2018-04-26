563b70538fae9c64d045c3a0cf04e5ea2ce42a6b
john kenneth furog <furogjohnkenneth@gmail.com>
2018-04-26 16:26:07 +0800

    updated pre build script

--- POC_BuildScript/POC_BuildScript.Android/appcenter-pre-build.sh
+++ POC_BuildScript/POC_BuildScript.Android/appcenter-pre-build.sh
@@ -8,11 +8,14 @@ APP_CONSTANT_FILE=$APPCENTER_SOURCE_DIRECTORY/POC_BuildScript/POC_BuildScript/Ut
 
 if [ -e "$APP_CONSTANT_FILE" ]
 then
+
+	echo "Updating key to $ENDPOINT in AppConstant.cs"
+    sed -i '' 's#endpoint = [a-z:./]*#endpoint = '$ENDPOINT'#' $APP_CONSTANT_FILE
+
     echo "Updating key to $APP_KEY in AppConstant.cs"
     sed -i '' 's#key = "[a-z:./]*"#key = "'$APP_KEY'"#' $APP_CONSTANT_FILE
 	
-	    echo "Updating key to $ENDPOINT in AppConstant.cs"
-    sed -i '' 's#endpoint = "[a-z:./]*"#endpoint = "'$ENDPOINT'"#' $APP_CONSTANT_FILE
+	    
 
     echo "File content:"
     cat $APP_CONSTANT_FILE
