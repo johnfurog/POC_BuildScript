7989f5afa5b179d4f273259b3f530850c9e183f8
john kenneth furog <furogjohnkenneth@gmail.com>
2018-04-26 18:05:06 +0800

    const to static

--- POC_BuildScript/POC_BuildScript/Util.cs
+++ POC_BuildScript/POC_BuildScript/Util.cs
@@ -7,7 +7,7 @@ namespace POC_BuildScript
     public class Util
     {
         public const string key = "Test";
-        public const Endpoint endpoint = Endpoint.FirstEndpoint;
+        public static Endpoint endpoint = Endpoint.FirstEndpoint;
     }
 
     public enum Endpoint
